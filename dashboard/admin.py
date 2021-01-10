9010083cd34e29fd408fb4674ddd50880f784d6a
Holly.Robertson <holly@hollyjrobertson.com>
2021-01-10 12:22:54 -0600

    Fixed Updates

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, Updates
+from django.contrib.auth.models import User
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'employee_id', 'mobile', 'email', 'hire_date', 'birthdate', 'role', 'team')
@@ -7,3 +8,8 @@ class EmployeeAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name', 'employee_id',  'mobile', 'email', 'hire_date', 'birthdate', 'role', 'team', 'avaya_login', 'ext')
 admin.site.register(Employee, EmployeeAdmin)
 
+class UpdatesAdmin(admin.ModelAdmin):
+    list_display = ('update_date','important', 'update' )
+    list_filter = ('update_date', 'important')
+    search_fields = ('update', 'important')
+admin.site.register(Updates, UpdatesAdmin)
